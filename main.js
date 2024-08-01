ef9869217fa4ac37489570c7dad3805e640ffcec
annnnangan <anna.ymngan@gmail.com>
2024-08-01 13:29:54 +0800

    Complete Location RWD

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./assets/scss/all.scss";
 
+// Collections Page
 const category = document.querySelectorAll(
   ".collection-category-header .category"
 );
@@ -82,5 +83,89 @@ if (sunglassesProductContainer) {
               </a> 
                 </div>
               </div>`;
+  }
+}
+
+// Location page
+const storeInfo = [
+  {
+    name: "台北中山旗艦店",
+    phone: "(02)000-1234",
+    schedule: "10:00-21:00",
+    address: "台北市中山區南京東路25巷2-1號",
+  },
+  {
+    name: "台北綠園店",
+    phone: "(02)000-2345",
+    schedule: "10:00-21:00",
+    address: "台北市中正區復興南路 132-1 號",
+  },
+  {
+    name: "台中清水旗艦店",
+    phone: "(02)000-1234",
+    schedule: "10:00-21:00",
+    address: "台中市清水區經南一路 23 號 8 樓",
+  },
+  {
+    name: "高雄中正形象店",
+    phone: "(07)000-2345",
+    schedule: "10:00-21:00",
+    address: "高雄市苓雅區中正路 38 號 12 樓",
+  },
+  {
+    name: "高雄夢時代店",
+    phone: "(07)000-1234",
+    schedule: "10:00-21:00",
+    address: "高雄市前鎮區中華一路 63 號 6 樓",
+  },
+];
+
+const locationContainer = document.querySelector(".location-list .row");
+
+if (locationContainer) {
+  for (let i = 0; i < storeInfo.length; i++) {
+    locationContainer.innerHTML += `<div class="column col-4 col-md-6 col-sm-12 d-flex">
+            <div class="card">
+              <img src="../assets/images/store-${i + 1}.png" alt="${
+      storeInfo[i].name
+    }" />
+              <div class="store-details">
+                <p class="location-name">${storeInfo[i].name}</p>
+                <ul class="location-contact d-flex flex-column">
+                  <li class="d-flex">
+                    <i class="material-icons-outlined"
+                      ><a href="tel:02000-1234">call</a></i
+                    >
+                    <p><a href="tel:${storeInfo[i].phone}">電話：${
+      storeInfo[i].phone
+    }</a></p>
+                  </li>
+                  <li class="d-flex">
+                    <i class="material-icons-outlined">schedule</i>
+                    <p>營業時間：${storeInfo[i].schedule}</p>
+                  </li>
+                  <li class="d-flex">
+                    <i class="material-icons-outlined"
+                      ><a
+                        href="https://www.google.com.hk/maps/search/${
+                          storeInfo[i].address
+                        }"
+                        >location_on</a
+                      ></i
+                    >
+                    <p>
+                      <a
+                        href="https://www.google.com.hk/maps/search/${
+                          storeInfo[i].address
+                        }"
+                        >地址：${storeInfo[i].address}</a
+                      >
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <a href="#" class="button">詳細資訊</a>
+            </div>
+          </div>`;
   }
 }
